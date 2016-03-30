438aedf715dc4f3c45d123289d289c8239ed6e85
l3lock <111.pk42@gmail.com>
2016-03-30 23:48:10 +0700

    test sprite

--- README.md
+++ README.md
@@ -1,15 +1,32 @@
-# game01
+Jungle Archer
+-------------
 
-# Command Run Game
+Jungle Archer is the first game that I create alone with my self
+
+
+----------
+
+*Command To Run Game*
+> **Run game with : **
+>
 $ mvn test -Pjava
 
-# Add Code pom.xml in ./core/ at line 22
+>----------
+> **Note :**
 
-# Code:   
+> Don't forget to add Code in " pom.xml in ./core/ " at line 22
+
+----------
+
+Code for pom.xml :
+		
 	<groupId>com.threerings</groupId>
 
    	<artifactId>tripleplay</artifactId>
 
    	<version>${playn.version}</version>
 
-# And Compile for Test
+----------
+
+
+
--- core/src/main/java/sut/game01/core/TestScreen.java
+++ core/src/main/java/sut/game01/core/TestScreen.java
@@ -10,6 +10,7 @@ import playn.core.ImageLayer;
 
 import react.UnitSlot;
 
+import sut.game01.core.character.Zealot;
 import tripleplay.game.Screen;
 import tripleplay.game.ScreenStack;
 
@@ -19,6 +20,8 @@ import tripleplay.ui.layout.AxisLayout;
 public class TestScreen extends Screen{
 
   public static final Font TITLE_FONT = graphics().createFont("Helvetica",Font.Style.PLAIN,24);
+
+  private Zealot z;
 
   private final ScreenStack ss;
   private final ImageLayer bg;
@@ -48,7 +51,16 @@ public class TestScreen extends Screen{
   @Override
   public void wasShown(){
     super.wasShown();
+
+    z = new Zealot(560f, 400f);
+    this.layer.add(z.layer());
+
     this.layer.add(bg);
     this.layer.add(backButton);
+  }
+
+  public void update (int delta) {
+    super.update(delta);
+    z.update(delta);
   }
 }
